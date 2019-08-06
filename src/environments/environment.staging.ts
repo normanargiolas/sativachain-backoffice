da31e59129ed67e7a23c009667001c943732a062
Samuele Collu <samuele.collu@gmail.com>
2019-08-06 11:53:36 +0200

    Staging environment config

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const apiBaseUrl = 'http://localhost:3000';
+const apiBaseUrl = 'https://my-json-server.typicode.com/ozisam/db';
 export const environment = {
     production: false,
     apiBaseUrl: apiBaseUrl,
